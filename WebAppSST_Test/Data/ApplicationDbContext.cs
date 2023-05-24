86098e6cf61a3c8e1511b6045f7a4ab118895fce
Sergeant101 <s.v.zagorodnev@gmail.com>
2023-05-24 09:57:34 +0500

    Добавьте файлы проекта.

---
+++ WebAppSST_Test/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppSST_Test.Models;
+
+namespace WebAppSST_Test.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebAppSST_Test/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppSST_Test.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? SunName { get; set; }
+    }
+}
